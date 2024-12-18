358700fc067de8526ecf20ab890c2d2b17bd4ee6
Oksana <oksanamp2014@gmail.com>
2024-12-18 11:09:23 +0200

    fix madal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,33 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    mobMenu: document.querySelector('.mob-menu'),
+
+    openModalBtn: document.querySelector('[modal-open-btn]'),
+    closeModalBtn: document.querySelector('.modal-close-btn'),
+    modalBackdrop: document.querySelector('.backdrop'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', () => {
+    refs.mobMenu.classList.add('is-open');
+  });
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-open');
-  }
+  refs.closeMenuBtn.addEventListener('click', () => {
+    refs.mobMenu.classList.remove('is-open');
+  });
+
+  refs.openModalBtn.addEventListener('click', () => {
+    refs.modalBackdrop.classList.add('is-open');
+  });
+
+  refs.closeModalBtn.addEventListener('click', () => {
+    refs.modalBackdrop.classList.remove('is-open');
+  });
+
+  refs.modalBackdrop.addEventListener('click', event => {
+    if (event.target === refs.modalBackdrop) {
+      refs.modalBackdrop.classList.remove('is-open');
+    }
+  });
 })();
